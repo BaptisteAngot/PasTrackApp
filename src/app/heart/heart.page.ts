308966d9369a48a69da676fc70be0de34d6ab38a
Angot <bat76640@gmail.com>
2020-02-24 10:53:45 +0100

    Test initialization Mapbox with Angular

--- src/app/heart/heart.page.ts
+++ src/app/heart/heart.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import { Chart } from "chart.js";
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
     selector: 'app-heart',
@@ -8,37 +8,39 @@ import { Chart } from "chart.js";
 })
 export class HeartPage implements OnInit {
 
-    @ViewChild("barCanvas") barCanvas: ElementRef;
+    // @ViewChild('barCanvas') barCanvas: ElementRef;
 
     private barChart: Chart;
 
-    constructor() {
+    constructor(
+        private barCanvas: ElementRef
+    ) {
     }
 
     ngOnInit() {
         this.barChart = new Chart(this.barCanvas.nativeElement, {
-            type: "bar",
+            type: 'bar',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [
                     {
-                        label: "# of Votes",
+                        label: '# of Votes',
                         data: [12, 19, 3, 5, 2, 3],
                         backgroundColor: [
-                            "rgba(255, 99, 132, 0.2)",
-                            "rgba(54, 162, 235, 0.2)",
-                            "rgba(255, 206, 86, 0.2)",
-                            "rgba(75, 192, 192, 0.2)",
-                            "rgba(153, 102, 255, 0.2)",
-                            "rgba(255, 159, 64, 0.2)"
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
                         ],
                         borderColor: [
-                            "rgba(255,99,132,1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)"
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
                         ],
                         borderWidth: 1
                     }
