28e66c6e29bfbe9b65a154a4ae748eb1fa39c825
Angot <bat76640@gmail.com>
2020-02-24 16:39:28 +0100

    Real-time journey GPS

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -17,5 +17,51 @@ export class MapPage implements OnInit {
 
   onMapLoad(map) {
     map.resize();
+    d3.json(
+        'https://docs.mapbox.com/mapbox-gl-js/assets/hike.geojson',
+        // tslint:disable-next-line:only-arrow-functions
+        function(err, data) {
+          if (err) { throw err; }
+
+          // save full coordinate list for later
+          const coordinates = data.features[0].geometry.coordinates;
+
+          // start by showing just the first coordinate
+          data.features[0].geometry.coordinates = [coordinates[0]];
+
+        // add it to the map
+          map.addSource('trace', { type: 'geojson', data });
+          map.addLayer({
+            id: 'trace',
+            type: 'line',
+            source: 'trace',
+            paint: {
+              'line-color': 'yellow',
+              'line-opacity': 0.75,
+              'line-width': 5
+            }
+          });
+
+          // setup the viewport
+          map.jumpTo({ center: coordinates[0], zoom: 14 });
+          map.setPitch(30);
+
+          // on a regular basis, add more coordinates from the saved list and update the map
+          let i = 0;
+          // tslint:disable-next-line:only-arrow-functions
+          const timer = window.setInterval(function() {
+            if (i < coordinates.length) {
+              data.features[0].geometry.coordinates.push(
+                  coordinates[i]
+              );
+              map.getSource('trace').setData(data);
+              map.panTo(coordinates[i]);
+              i++;
+            } else {
+              window.clearInterval(timer);
+            }
+          }, 50);
+        }
+    );
   }
 }
