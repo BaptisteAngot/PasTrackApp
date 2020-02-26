6cd1d361bb1f823677ab8b67a24accf0fb9888df
Angot <bat76640@gmail.com>
2020-02-26 09:39:01 +0100

    Add D3 to the map to follow the circuit of the customer

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-map',
@@ -17,51 +18,52 @@ export class MapPage implements OnInit {
 
   onMapLoad(map) {
     map.resize();
-    d3.json(
-        'https://docs.mapbox.com/mapbox-gl-js/assets/hike.geojson',
-        // tslint:disable-next-line:only-arrow-functions
-        function(err, data) {
-          if (err) { throw err; }
+    d3.json('https://angotbaptiste.com/test')
+        .then((data) => {
+            // save full coordinate list for later
+            // @ts-ignore
+            const coordinates = data.features[0].geometry.coordinates;
 
-          // save full coordinate list for later
-          const coordinates = data.features[0].geometry.coordinates;
+            // start by showing just the first coordinate
+            // @ts-ignore
+            data.features[0].geometry.coordinates = [coordinates[0]];
 
-          // start by showing just the first coordinate
-          data.features[0].geometry.coordinates = [coordinates[0]];
+            // add it to the map
+            map.addSource('trace', { type: 'geojson', data });
+            map.addLayer({
+                id: 'trace',
+                type: 'line',
+                source: 'trace',
+                paint: {
+                    'line-color': 'yellow',
+                    'line-opacity': 0.75,
+                    'line-width': 5
+                }
+            });
 
-        // add it to the map
-          map.addSource('trace', { type: 'geojson', data });
-          map.addLayer({
-            id: 'trace',
-            type: 'line',
-            source: 'trace',
-            paint: {
-              'line-color': 'yellow',
-              'line-opacity': 0.75,
-              'line-width': 5
-            }
-          });
+            // setup the viewport
+            map.jumpTo({ center: coordinates[0], zoom: 14 });
+            map.setPitch(30);
 
-          // setup the viewport
-          map.jumpTo({ center: coordinates[0], zoom: 14 });
-          map.setPitch(30);
-
-          // on a regular basis, add more coordinates from the saved list and update the map
-          let i = 0;
-          // tslint:disable-next-line:only-arrow-functions
-          const timer = window.setInterval(function() {
-            if (i < coordinates.length) {
-              data.features[0].geometry.coordinates.push(
-                  coordinates[i]
-              );
-              map.getSource('trace').setData(data);
-              map.panTo(coordinates[i]);
-              i++;
-            } else {
-              window.clearInterval(timer);
-            }
-          }, 50);
-        }
-    );
+            // on a regular basis, add more coordinates from the saved list and update the map
+            let i = 0;
+            // tslint:disable-next-line:only-arrow-functions
+            const timer = window.setInterval(function() {
+                if (i < coordinates.length) {
+                    // @ts-ignore
+                    data.features[0].geometry.coordinates.push(
+                        coordinates[i]
+                    );
+                    map.getSource('trace').setData(data);
+                    map.panTo(coordinates[i]);
+                    i++;
+                } else {
+                    window.clearInterval(timer);
+                }
+            }, 50);
+        })
+        .catch((err) => {
+            throw err;
+        });
   }
 }
